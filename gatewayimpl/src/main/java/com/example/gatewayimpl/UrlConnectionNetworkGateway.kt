60f08144cf3d0c05bb8a645bfc7c514a47d4af34
Prashant Rathore <prashant.rathore>
2019-11-30 20:36:41 +0530

    Used callable instead of create as it will only return single response

--- gatewayimpl/src/main/java/com/example/gatewayimpl/UrlConnectionNetworkGateway.kt
+++ gatewayimpl/src/main/java/com/example/gatewayimpl/UrlConnectionNetworkGateway.kt
@@ -10,12 +10,9 @@ import java.net.URL
 class UrlConnectionNetworkGateway(context: Context) : NetworkGateway {
 
     override fun loadUrl(url: String): Observable<Result<ByteArray>> {
-        return Observable.create<Result<ByteArray>> { emmiter ->
-
+        return Observable.fromCallable {
             val openConnection = URL(url).openConnection() as HttpURLConnection
-            val responseCode = openConnection.responseCode
-            emmiter.onNext(Result.success(("" + responseCode).toByteArray(Charsets.UTF_8)))
-            
+            Result.success(("" + openConnection.responseCode).toByteArray(Charsets.UTF_8))
         }.onErrorReturn {
             it.printStackTrace()
             Result.failure(it)
